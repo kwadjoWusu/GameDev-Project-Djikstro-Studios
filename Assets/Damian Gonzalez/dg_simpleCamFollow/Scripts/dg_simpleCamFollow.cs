017680425a08b75c143c2e552ad7b452fbb9395e
EwuramaBoateng-Yeboah <151070805+EwuramaBoateng-Yeboah@users.noreply.github.com>
2025-04-08 16:11:18 +0000

    commit new camera

--- Assets/Damian Gonzalez/dg_simpleCamFollow/Scripts/dg_simpleCamFollow.cs
+++ Assets/Damian Gonzalez/dg_simpleCamFollow/Scripts/dg_simpleCamFollow.cs
@@ -1,30 +1,53 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class dg_simpleCamFollow : MonoBehaviour
 {
     public Transform target;
-    [Range(1f,40f)] public float laziness = 10f;
+    [Range(1f, 40f)] public float laziness = 10f;
     public bool lookAtTarget = true;
     public bool takeOffsetFromInitialPos = true;
     public Vector3 generalOffset;
-    Vector3 whereCameraShouldBe;
-    bool warningAlreadyShown = false;
+    
+    private Vector3 whereCameraShouldBe;
+    private Quaternion initialRotation;
+    private bool warningAlreadyShown = false;
 
-    private void Start() {
-        if (takeOffsetFromInitialPos && target != null) generalOffset = transform.position - target.position;
+    private void Start()
+    {
+        if (target != null)
+        {
+            if (takeOffsetFromInitialPos)
+            {
+                generalOffset = transform.position - target.position;
+            }
+            
+            // Store the initial rotation between camera and target
+            initialRotation = transform.rotation;
+        }
     }
 
     void FixedUpdate()
     {
-        if (target != null) {
+        if (target != null)
+        {
             whereCameraShouldBe = target.position + generalOffset;
             transform.position = Vector3.Lerp(transform.position, whereCameraShouldBe, 1 / laziness);
-
-            if (lookAtTarget) transform.LookAt(target);
-        } else {
-            if (!warningAlreadyShown) {
+            
+            if (lookAtTarget)
+            {
+                // Option 1: Use the initial relative rotation
+                transform.rotation = initialRotation;
+                
+                // Option 2: Look at a point that's offset from the target
+                // Vector3 lookAtPoint = target.position + new Vector3(0, generalOffset.y, 0);
+                // transform.LookAt(lookAtPoint);
+            }
+        }
+        else
+        {
+            if (!warningAlreadyShown)
+            {
                 Debug.Log("Warning: You should specify a target in the simpleCamFollow script.", gameObject);
                 warningAlreadyShown = true;
             }
